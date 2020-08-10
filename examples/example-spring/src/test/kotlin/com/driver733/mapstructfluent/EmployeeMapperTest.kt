40f8e362b312a9c9c5cd25ce661185cb97a71b06
Mikhail Yakushin <driver733@gmail.com>
2020-08-10 22:10:10 +0300

    fix: tests

--- examples/example-spring/src/test/kotlin/com/driver733/mapstructfluent/EmployeeMapperTest.kt
+++ examples/example-spring/src/test/kotlin/com/driver733/mapstructfluent/EmployeeMapperTest.kt
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import toEmployeeView
 
 @SpringBootTest
 class EmployeeMapperTest @Autowired constructor(
